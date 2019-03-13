05034ec52570337c4cc04c8a11cc46d6a36f9d77
WaydeBelgrave <WaydeBelgrave@users.noreply.github.com>
2019-03-13 11:51:59 -0400

    Lab 5

--- src/pointers.cpp
+++ src/pointers.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 using namespace std;
+void myswap();
 
-void myswap(int * ptr1, int* ptr2)
+void myswap(int  ptr1, int ptr2)
 {
   auto temp = ptr1;
   ptr1 = ptr2;
@@ -20,9 +21,8 @@ int main()
 
 //swap(a,b); //why does theirs work????   
  
-  myswap(a, b); // but mine doesn't ?!?!?!?!!!???
+  myswap(a,b); // but mine doesn't ?!?!?!?!!!???
 
   cout<<"a = "<<a<<", b = "<<b<<endl;
-
 
 }
